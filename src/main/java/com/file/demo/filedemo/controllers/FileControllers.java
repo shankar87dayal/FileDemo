5f6138f2e1b33f3f8faffb6c79df9748977515af
shankar87dayal <shankar.87.dayal@gmail.com>
2022-04-29 15:29:24 +0530

    One of the simplest way to serve images in Spring Boot Project Serving Files

--- src/main/java/com/file/demo/filedemo/controllers/FileControllers.java
+++ src/main/java/com/file/demo/filedemo/controllers/FileControllers.java
@@ -1,11 +1,19 @@
 package com.file.demo.filedemo.controllers;
 
+import java.awt.PageAttributes.MediaType;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,5 +53,33 @@ public class FileControllers {
 	
 	}
 	
+	// methods to serve files
+	@GetMapping( value ="/profiles/{imageName}",produces = org.springframework.http.MediaType.IMAGE_JPEG_VALUE)
+	public void downloadImage(
+			@PathVariable("imageName") String imageName,
+			HttpServletResponse response
+			) throws IOException 
+	{
+	 
+		InputStream resource =this.fileService.getResource(path, imageName);
+		response.setContentType(org.springframework.http.MediaType.IMAGE_JPEG_VALUE);
+		StreamUtils.copy(resource, response.getOutputStream());
+		
+		
+	}
+	
+	
+	//localhost:8080/images/abc.png
 	
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/file/demo/filedemo/services/FileService.java
+++ src/main/java/com/file/demo/filedemo/services/FileService.java
@@ -1,7 +1,9 @@
 package com.file.demo.filedemo.services;
  
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,4 +12,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 	String uploadImage(String path, MultipartFile file) throws IOException;
+	InputStream getResource(String path, String fileName) throws FileNotFoundException;
 }
